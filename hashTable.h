8313ddf9c9ab8459a64957c8abc34c2aca7fe499
Andrey Andreyev <aandreyev@corvidtec.com>
2016-09-15 16:04:03 -0400

    commit to work from home, will rebase later

--- hashTable.h
+++ hashTable.h
@@ -1,13 +1,16 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 #include <unordered_map>
+using namespace std;
 
 class stringHash{
     private:
-        unordered_map<int,char> hash_;
+        unordered_map<int,char*> hash_;
     public:
         stringHash();
         ~stringHash();
+        void insert(int,char*);
+        char* find(int);
 
 
 }
