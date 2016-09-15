f3d7b444056e20ff86febf629b699ded081804bb
Andrey Andreyev <aandreyev@corvidtec.com>
2016-09-15 14:46:23 -0400

    initial commit

---
+++ hashTable.h
@@ -0,0 +1,16 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+#include <unordered_map>
+
+class stringHash{
+    private:
+        unordered_map<int,char> hash_;
+    public:
+        stringHash();
+        ~stringHash();
+
+
+}
+
+
+#endif
