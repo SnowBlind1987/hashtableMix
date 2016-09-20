a192d3249ed59a188103e7d2a8800028216a6cf8
Andrey Andreyev <aandreyev@corvidtec.com>
2016-09-20 18:04:23 -0400

    module done, time to debug

--- lib/module_hashtable.f90
+++ lib/module_hashtable.f90
@@ -18,7 +18,7 @@ INTERFACE
         type(c_ptr),intent(in)::itself
     END SUBROUTINE
 
-   SUBROUTINE hashTable__init(itself) bind(C,name="hashTable__init")
+   SUBROUTINE hashTable__clear(itself) bind(C,name="hashTable__init")
         USE iso_c_binding, only: c_ptr
         type(c_ptr),intent(in)::itself
    END SUBROUTINE hashTable__init 
@@ -27,16 +27,56 @@ INTERFACE
         use iso_c_binding,only:c_ptr,c_char,c_int
         type(c_ptr)::itself
         integer(c_int),value,intent(in)::key
-        character(c_char),intent(in)::value
+        character(c_char),intent(in)::value(*)
    END SUBROUTINE hashTable_insert
 
    SUBROUTINE hashTable__find(itself,key,value,ierr) bind(C,name="hashTable_find")
         use iso_c_binding,only:c_ptr,c_char,c_int
         type(c_ptr),intent(in)::itself
         integer(c_int),value,intent(in)::key
-        character(c_char),intent(out)::value
+        character(c_char),intent(out)::value(*)
         integer(c_int),intent(out)::ierr
    END SUBROUTINE hashTable_find
 END INTERFACE
     
+
+CONTAINS
+
+subroutine stringHashNew(itself)
+    type(string_hash),intent(inout)::itself
+    call hashTable__new(itself&hash_ptr)
+end subroutine stringHashNew
+
+subroutine stringHashDel(itself)
+    type(string_hash),intent(inout)::itself
+    call hashTable__delete(itself%hash_ptr)
+    itself%hash_ptr=c_ptr_null
+end subroutine stringHashDel
+
+subroutine stringHashClear(itself)
+    type(string_hash),intent(inout)::itself
+    call hashTable__init(itself%hash_ptr)
+end subroutine stringHashClear
+
+subroutine stringHashInsert(itself,key,value)
+    use iso_c_binding,only:c_char
+    type(string_hash),intent(in)::itself
+    integer,intent(in)::key
+    character(len=*),intent(in)::value
+    character(type=c_char,len=33)::c_str
+    c_str=adjustl(trim(value))//"/0"
+    call hashTable__insert(itself%hash_ptr,key,value)
+end subroutine stringHashInsert
+
+subroutine stringHashFind(itself,key,value,ierr)
+    implicit none
+    use iso_c_binding,only:c_char
+    type(string_hash),intent(in)::itself
+    integer,intent(in)::key
+    character(len=*),intent(out)::value
+    integer,intent(out)::ierr
+    character(type=c_char,len=33)::c_str
+    call stringHash__find(itself%hash_ptr,key,c_str,ierr)
+    !need to set value to c_string
+end subroutine stringHashFind
 end type end module string_hash
