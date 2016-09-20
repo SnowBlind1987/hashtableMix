fe539428e7079ba0ef682c24aaab527dfd006b57
Andrey Andreyev <andreyev.andrey@gmail.com>
2016-09-19 20:39:13 -0400

    added error check into the hashtable

--- main.cpp
+++ main.cpp
@@ -3,14 +3,19 @@
 #include "hashTable.h"
 //#include <unordered_map>
 
-using namespace std;
-
+using std::cout;
+using std::endl;
 int main(){
     hashTable myHash;
     char value[] ="testing";
     int key=2;
     myHash.insert(key,value);
 	char* output;
-    int ierr=myHash.find(key,output);
-	cout<<output<<endl;
+    int ierr=myHash.find(1,output);
+	if (ierr!=-1){
+		cout<<output<<endl;
+	}
+	else{
+		cout<<"key not found"<<endl;
+	}
 }
