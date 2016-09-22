ca94230e36fad36c3742aea7ab92578bc6e5bc84
Andrey Andreyev <aandreyev@corvidtec.com>
2016-09-22 17:36:45 -0400

    can't delete invalid pointer

--- test.f90
+++ test.f90
@@ -5,6 +5,7 @@ program test
     character(len=30):: test_val
     integer::key,ierr
     call hashNew(hash)
-    call hashClear(hash)
+    !call hashClear(hash)
+    call hashDel(hash)
     write(*,*) "past where I'm supposed to be" 
 end program test
