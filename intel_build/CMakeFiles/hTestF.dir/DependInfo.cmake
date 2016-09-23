b8b632ccbfbbd6649346fde8a0a0f3535fb9c7a1
Andrey Andreyev <aandreyev@corvidtec.com>
2016-09-23 15:31:01 -0400

    branch with char allocation, crashes

---
+++ intel_build/CMakeFiles/hTestF.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/shelf7/aandreyev/code_practice/hashtableMix/test.f90" "/home/shelf7/aandreyev/code_practice/hashtableMix/intel_build/CMakeFiles/hTestF.dir/test.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "Intel")
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  "../include"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/shelf7/aandreyev/code_practice/hashtableMix/intel_build/lib/CMakeFiles/myHashF.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "/home/shelf7/aandreyev/code_practice/hashtableMix/intel_build/include")
---
+++ intel_build/lib/CMakeFiles/myHashF.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,6 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "../include/test_hash.mod"
+  "../include/TEST_HASH.mod"
+  "CMakeFiles/myHashF.dir/test_hash.mod.stamp"
+  )
